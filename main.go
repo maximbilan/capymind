b71b1032aa4ad44ffd2b4a6559c8e51692e162e4
Maksym Bilan <>
2024-08-05 13:08:07 +0300

    server for testing

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"capymind/cloud"
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Starting capymind...")
+	http.HandleFunc("/", cloud.Handler)
+	fmt.Println("Starting server on localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
