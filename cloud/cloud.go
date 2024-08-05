fc73f78b14ff5b1afdb0aff8223bc6fb82929eac
Maksym Bilan <>
2024-08-05 15:57:18 +0300

    Manual telegram bot handling

--- cloud/cloud.go
+++ cloud/cloud.go
@@ -3,27 +3,81 @@ package cloud
 import (
 	"encoding/json"
 	"fmt"
-	"html"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"strconv"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 )
+
+type Update struct {
+	UpdateId int     `json:"update_id"`
+	Message  Message `json:"message"`
+}
+
+type Message struct {
+	Text string `json:"text"`
+	Chat Chat   `json:"chat"`
+}
+
+type Chat struct {
+	Id int `json:"id"`
+}
 
 func init() {
 	functions.HTTP("handler", Handler)
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	var d struct {
-		Name string `json:"name"`
-	}
-	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Capy is here")
+	var update, err = parseTelegramRequest(r)
+	if err != nil {
+		log.Printf("error parsing update, %s", err.Error())
 		return
 	}
-	if d.Name == "" {
-		fmt.Fprint(w, "Capy is here")
-		return
+
+	fmt.Println("Server received message: ", update.Message.Text)
+
+	var telegramResponseBody, errTelegram = sendTextToTelegramChat(update.Message.Chat.Id, "Now I can respond to you!")
+	if errTelegram != nil {
+		log.Printf("got error %s from telegram, reponse body is %s", errTelegram.Error(), telegramResponseBody)
 	}
-	fmt.Fprintf(w, "Hello, %s!", html.EscapeString(d.Name))
+}
+
+func parseTelegramRequest(r *http.Request) (*Update, error) {
+	var update Update
+	if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
+		log.Printf("could not decode incoming update %s", err.Error())
+		return nil, err
+	}
+	return &update, nil
+}
+
+func sendTextToTelegramChat(chatId int, text string) (string, error) {
+	log.Printf("Sending %s to chat_id: %d", text, chatId)
+	var telegramApi string = "https://api.telegram.org/bot" + os.Getenv("TELEGRAM_BOT_TOKEN") + "/sendMessage"
+	response, err := http.PostForm(
+		telegramApi,
+		url.Values{
+			"chat_id": {strconv.Itoa(chatId)},
+			"text":    {text},
+		})
+
+	if err != nil {
+		log.Printf("error when posting text to the chat: %s", err.Error())
+		return "", err
+	}
+	defer response.Body.Close()
+
+	var bodyBytes, errRead = ioutil.ReadAll(response.Body)
+	if errRead != nil {
+		log.Printf("error in parsing telegram answer %s", errRead.Error())
+		return "", err
+	}
+	bodyString := string(bodyBytes)
+	log.Printf("Body of Telegram Response: %s", bodyString)
+
+	return bodyString, nil
 }
