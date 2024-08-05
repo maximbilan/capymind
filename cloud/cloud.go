493d95fa5823fd5661ab3bb761d5dbb37bb9606d
Maksym Bilan <>
2024-08-05 13:01:14 +0300

    cloud

---
+++ cloud/cloud.go
@@ -0,0 +1,29 @@
+package cloud
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+func init() {
+	functions.HTTP("Handler", Handler)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	var d struct {
+		Name string `json:"name"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		fmt.Fprint(w, "Capy is here")
+		return
+	}
+	if d.Name == "" {
+		fmt.Fprint(w, "Capy is here")
+		return
+	}
+	fmt.Fprintf(w, "Hello, %s!", html.EscapeString(d.Name))
+}
