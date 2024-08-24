7cc000849a38d17caed1cf88f8bb75cebac14d78
Maksym Bilan <>
2024-08-24 18:30:00 +0300

    new command

--- internal/bot/command.go
+++ internal/bot/command.go
@@ -3,9 +3,10 @@ package bot
 type Command string
 
 const (
-	Start Command = "/start"
-	Note  Command = "/note"
-	Last  Command = "/last"
-	Info  Command = "/info"
-	Help  Command = "/help"
+	Start  Command = "/start"
+	Note   Command = "/note"
+	Last   Command = "/last"
+	Locale Command = "/locale"
+	Info   Command = "/info"
+	Help   Command = "/help"
 )
