44132002b5ae5599bc51a42bbbcd0d87abebc0ae
Maksym Bilan <>
2024-08-27 14:11:57 +0300

    split telegram types into separate files

---
+++ internal/telegram/callback_query.go
@@ -0,0 +1,8 @@
+package telegram
+
+type CallbackQuery struct {
+	ID      string   `json:"id"`
+	From    *User    `json:"from"`
+	Message *Message `json:"message,omitempty"`
+	Data    string   `json:"data,omitempty"`
+}
---
+++ internal/telegram/message.go
@@ -0,0 +1,14 @@
+package telegram
+
+type Message struct {
+	Text string `json:"text"`
+	Chat Chat   `json:"chat"`
+	From User   `json:"from"`
+	Date int    `json:"date"`
+}
+
+type SendMessageRequest struct {
+	ChatID      int                  `json:"chat_id"`
+	Text        string               `json:"text"`
+	ReplyMarkup InlineKeyboardMarkup `json:"reply_markup"`
+}
