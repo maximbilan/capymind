d8ea3f6d376561cda081e00c3b98a58a70b76f84
Maksym Bilan <>
2024-08-27 14:07:02 +0300

    functions for callback data

--- internal/telegram/callback_parameter.go
+++ internal/telegram/callback_parameter.go
@@ -1,5 +1,7 @@
 package telegram
 
+import "strings"
+
 type CallbackParameter string
 
 const (
@@ -8,4 +10,16 @@ const (
 
 func (c CallbackParameter) String() string {
 	return string(c)
+}
+
+func ParseCallbackParameter(input string, param CallbackParameter) (*string, bool) {
+	parts := strings.Split(input, "_")
+	if len(parts) == 2 && parts[0] == param.String() {
+		return &parts[1], true
+	}
+	return nil, false
+}
+
+func GetParameterFmt(param CallbackParameter, value string) string {
+	return param.String() + "_" + value
 }
