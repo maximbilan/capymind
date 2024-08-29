d0658a86b33a90dcb84f46fce1c3e8b390db482a
Maksym Bilan <>
2024-08-29 13:59:50 +0300

    refactoring

--- internal/firestore/firestore.go
+++ internal/firestore/firestore.go
@@ -82,36 +82,3 @@ func LastNote(ctx context.Context, client *firestore.Client, userId string) (*No
 	}
 	return nil, nil
 }
-
-func getUser(ctx context.Context, client *firestore.Client, userId string) (*User, error) {
-	doc, err := client.Collection(users.String()).Doc(userId).Get(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	var user User
-	doc.DataTo(&user)
-	return &user, nil
-}
-
-func UserLocale(ctx context.Context, client *firestore.Client, userId string) (*string, error) {
-	user, err := getUser(ctx, client, userId)
-	if err != nil {
-		return nil, err
-	}
-	return &user.Locale, nil
-}
-
-func UpdateUserLocale(ctx context.Context, client *firestore.Client, userId string, locale string) error {
-	_, err := client.Collection(users.String()).Doc(userId).Set(ctx, map[string]interface{}{
-		"locale": locale,
-	}, firestore.MergeAll)
-	return err
-}
-
-func UpdateUserTimezone(ctx context.Context, client *firestore.Client, userId string, secondsFromUTC int) error {
-	_, err := client.Collection(users.String()).Doc(userId).Set(ctx, map[string]interface{}{
-		"secondsFromUTC": secondsFromUTC,
-	}, firestore.MergeAll)
-	return err
-}
--- internal/firestore/user.go
+++ internal/firestore/user.go
@@ -1,7 +1,46 @@
 package firestore
 
+import (
+	"context"
+
+	"cloud.google.com/go/firestore"
+)
+
 type User struct {
 	ID     string `firestore:"id"`
 	Name   string `firestore:"name"`
 	Locale string `firestore:"locale"`
+}
+
+func getUser(ctx context.Context, client *firestore.Client, userId string) (*User, error) {
+	doc, err := client.Collection(users.String()).Doc(userId).Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var user User
+	doc.DataTo(&user)
+	return &user, nil
+}
+
+func UserLocale(ctx context.Context, client *firestore.Client, userId string) (*string, error) {
+	user, err := getUser(ctx, client, userId)
+	if err != nil {
+		return nil, err
+	}
+	return &user.Locale, nil
+}
+
+func UpdateUserLocale(ctx context.Context, client *firestore.Client, userId string, locale string) error {
+	_, err := client.Collection(users.String()).Doc(userId).Set(ctx, map[string]interface{}{
+		"locale": locale,
+	}, firestore.MergeAll)
+	return err
+}
+
+func UpdateUserTimezone(ctx context.Context, client *firestore.Client, userId string, secondsFromUTC int) error {
+	_, err := client.Collection(users.String()).Doc(userId).Set(ctx, map[string]interface{}{
+		"secondsFromUTC": secondsFromUTC,
+	}, firestore.MergeAll)
+	return err
 }
