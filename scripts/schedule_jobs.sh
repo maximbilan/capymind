d25a2673ac5fc4817cf039f1eca0730226ef308d
Maksym Bilan <>
2024-09-05 17:34:53 +0300

    polishing

--- scripts/schedule_jobs.sh
+++ scripts/schedule_jobs.sh
@@ -4,7 +4,7 @@ FUNCTION_NAME="schedule"
 PROJECT_ID=$CAPY_PROJECT_ID
 REGION=$CAPY_SERVER_REGION
 
-gcloud scheduler jobs create http schedule-message-1 \
+gcloud scheduler jobs create http schedule-morning-messages \
   --project $PROJECT_ID \
   --uri="https://$REGION-$PROJECT_ID.cloudfunctions.net/$FUNCTION_NAME" \
   --schedule="0 0 * * *" \
@@ -12,7 +12,7 @@ gcloud scheduler jobs create http schedule-message-1 \
   --http-method=GET \
   --location=$REGION
 
-gcloud scheduler jobs create http schedule-message-2 \
+gcloud scheduler jobs create http schedule-evening-messages \
   --project $PROJECT_ID \
   --uri="https://$REGION-$PROJECT_ID.cloudfunctions.net/$FUNCTION_NAME" \
   --schedule="0 12 * * *" \
