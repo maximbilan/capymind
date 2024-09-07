f193fb2e3e3006a3aa4562da24fb0d28a3ddd1f8
Maksym Bilan <maximb.mail@gmail.com>
2024-09-07 12:10:44 +0300

    Delete jobs before rescheduling (#17)
    Co-authored-by: Maksym Bilan <>

--- scripts/schedule_jobs.sh
+++ scripts/schedule_jobs.sh
@@ -4,14 +4,27 @@ FUNCTION_NAME="schedule"
 PROJECT_ID=$CAPY_PROJECT_ID
 REGION=$CAPY_SERVER_REGION
 
-gcloud scheduler jobs create http schedule-morning-messages \
+JOB1="schedule-morning-messages"
+JOB2="schedule-evening-messages"
+
+gcloud scheduler jobs delete $JOB1 \
+  --project $PROJECT_ID \
+  --location=$REGION \
+  --quiet
+
+gcloud scheduler jobs delete $JOB2 \
+  --project $PROJECT_ID \
+  --location=$REGION \
+  --quiet
+
+gcloud scheduler jobs create http $JOB1 \
   --project $PROJECT_ID \
   --uri="https://$REGION-$PROJECT_ID.cloudfunctions.net/$FUNCTION_NAME?type=morning" \
   --schedule="0 0 * * *" \
   --http-method=GET \
   --location=$REGION
 
-gcloud scheduler jobs create http schedule-evening-messages \
+gcloud scheduler jobs create http $JOB2 \
   --project $PROJECT_ID \
   --uri="https://$REGION-$PROJECT_ID.cloudfunctions.net/$FUNCTION_NAME?type=evening" \
   --schedule="0 12 * * *" \
