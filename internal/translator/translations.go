75d6d20b828740c72c3adfda762084bf9b468ed3
Maksym Bilan <>
2024-09-07 12:37:26 +0300

    new texts

--- internal/translator/translations.go
+++ internal/translator/translations.go
@@ -2,9 +2,10 @@ package translator
 
 const translationsJSON = `{
     "en": {
-        "welcome": "Welcome to CapyMind! Your personal journal for mental health notes is here to help you on your journey. Reflect on your thoughts and emotions, use reminders to stay on track, and explore therapy insights to deepen your self-awareness.",
-        "start_note" : "Please input your thoughts and feelings in the text field and send them to me. Your personal reflections will be safely stored in your journal.",
-        "finish_note" : "Your thoughts have been successfully stored. Thank you for sharing with CapyMind. Remember, every note is a step towards better mental well-being.",
+        "welcome": "Welcome to CapyMind 👋 Your personal journal for mental health notes is here to help you on your journey. Reflect on your thoughts and emotions, use reminders to stay on track, and explore therapy insights to deepen your self-awareness.",
+        "info": "Remove this text",
+        "start_note" : "Share your thoughts and feelings by entering them in the text field and sending them my way. Your personal reflections will be securely saved in your journal 👇",
+        "finish_note" : "Your thoughts have been successfully saved. Thank you for trusting CapyMind. Remember, each note is a step forward on your journey to better mental well-being 🙂",
         "your_last_note": "Here is your last note: ",
         "no_notes": "You have not made any entries yet. Start by sharing your thoughts and feelings with CapyMind.",
         "commands_hint": "You can use the following commands to interact with CapyMind:\n/start - Start the bot\n/note - Make a journal entry\n/last - View your last entry\n/locale - Set up a locale\n/timezone - Set up a timezone\n/info - Learn more about CapyMind\n/help - Get help with using CapyMind\n",
@@ -20,10 +21,10 @@ const translationsJSON = `{
         "ai_analysis_prompt": "You're a professional therapist at Capymind. A patient comes to you with the following notes. What would you say to them? (Shortly summary) Notes: "
     },
     "uk": {
-        "welcome": "Ласкаво просимо до CapyMind! Ваш особистий журнал для записів про психічне здоров'я тут, щоб допомогти вам на вашому шляху. Рефлексуйте над своїми думками та емоціями, використовуйте нагадування, щоб залишатися на шляху, та досліджуйте інсайти терапії, щоб поглибити свою самосвідомість.",
-        "info": "CapyMind допомагає вам вести особистий журнал вашого психічного здоров'я. Ви можете записувати свої думки та почуття, відстежувати свій емоційний шлях і розмірковувати над своїм прогресом з часом. Використовуйте команди, щоб почати робити записи та зробити крок до самосвідомості та психічного благополуччя.",
-        "start_note" : "Будь ласка, введіть свої думки та почуття в текстове поле та надішліть їх мені. Ваші особисті роздуми будуть надійно збережені в вашому журналі.",
-        "finish_note" : "Ваші думки успішно збережені. Дякую за те, що поділилися з CapyMind. Пам'ятайте, кожен запис - це крок до кращого психічного благополуччя.",
+        "welcome": "Ласкаво просимо до CapyMind 👋 Ваш особистий журнал для записів про психічне здоров'я тут, щоб допомогти вам на вашому шляху. Рефлексуйте над своїми думками та емоціями, використовуйте нагадування, щоб залишатися на шляху, та досліджуйте інсайти терапії, щоб поглибити свою самосвідомість.",
+        "info": "Remove this text",
+        "start_note": "Поділіться своїми думками та почуттями, введіть їх у текстове поле та надішліть мені. Ваші особисті роздуми будуть безпечно збережені у вашому журналі 👇",
+        "finish_note": "Ваші думки успішно збережені. Дякуємо вам за довіру CapyMind. Пам'ятайте, кожен запис - це крок вперед на вашому шляху до кращого психічного самопочуття 🙂",
         "your_last_note": "Ось ваш останній запис: ",
         "no_notes": "Ви ще не зробили жодного запису. Почніть ділитися своїми думками та почуттями з CapyMind.",
         "commands_hint": "Ви можете використовувати наступні команди для взаємодії з CapyMind:\n/start - Запустити бота\n/note - Зробити запис в журнал\n/last - Переглянути ваш останній запис\n/locale - Налаштувати мову\n/timezone - Налаштувати часовий пояс\n/info - Дізнатися більше про CapyMind\n/help - Отримати допомогу з використання CapyMind\n",
