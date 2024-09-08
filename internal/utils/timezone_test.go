89ffab68942857b133c56417b3dd797b4b18d440
Maksym Bilan <>
2024-09-08 14:48:23 +0300

    more tests

--- internal/utils/timezone_test.go
+++ internal/utils/timezone_test.go
@@ -13,6 +13,33 @@ func TestTimezoneParameter(t *testing.T) {
 	param := GetTimezoneParameter(info)
 
 	if want != param {
-		t.Fatalf("Wrong parameter. %s doesn't equal %s", want, param)
+		t.Fatalf("Incorrect parameter. %s doesn't equal %s", want, param)
+	}
+}
+
+func TestParseTimezone(t *testing.T) {
+	want, ok := ParseTimezone("timezone_3600")
+	if !ok || *want != 3600 {
+		t.Fatalf("Incorrect timezone. %v doesn't equal %v", want, 3600)
+	}
+
+	want, ok = ParseTimezone("timezone_abc")
+	if ok {
+		t.Fatalf("Incorrect timezone. %v is not nil", want)
+	}
+
+	want, ok = ParseTimezone("timezone")
+	if ok {
+		t.Fatalf("Incorrect timezone. %v is not nil", want)
+	}
+
+	want, ok = ParseTimezone("timezone_")
+	if ok {
+		t.Fatalf("Incorrect timezone. %v is not nil", want)
+	}
+
+	want, ok = ParseTimezone("timezone_0")
+	if !ok || *want != 0 {
+		t.Fatalf("Incorrect timezone. %v doesn't equal %v", want, 0)
 	}
 }
