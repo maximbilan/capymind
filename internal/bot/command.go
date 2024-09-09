edb055a47fd0431fbc8ffba529cbb51c19fc7642
Maksym Bilan <>
2024-09-09 17:58:43 +0300

    command design

--- internal/bot/command.go
+++ internal/bot/command.go
@@ -1,5 +1,7 @@
 package bot
 
+import "strings"
+
 type Command string
 
 const (
@@ -10,4 +12,29 @@ const (
 	Language Command = "/language"
 	Timezone Command = "/timezone"
 	Help     Command = "/help"
+	Plain    Command = "" // No command, just plain text
 )
+
+var commandsWithParams = []Command{Language, Timezone}
+
+func (c Command) HasParam() bool {
+	for _, command := range commandsWithParams {
+		if c == command {
+			return true
+		}
+	}
+	return false
+}
+
+func ParseCommand(input string) (Command, *string) {
+	if len(input) == 0 || input[0] != '/' {
+		return Plain, nil
+	}
+
+	parts := strings.Split(input, " ")
+	if len(parts) == 1 {
+		return Command(parts[0]), nil
+	} else {
+		return Command(parts[0]), &parts[1]
+	}
+}
