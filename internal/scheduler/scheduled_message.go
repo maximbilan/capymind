f4f00c3f0170182361869a7d3b3bd8c447871eda
Maksym Bilan <>
2024-09-09 08:48:53 +0300

    fixing model

--- internal/scheduler/scheduled_message.go
+++ internal/scheduler/scheduled_message.go
@@ -3,7 +3,7 @@ package scheduler
 import "github.com/capymind/internal/translator"
 
 type ScheduledMessage struct {
-	ChatId int               `json:"chatId"`
+	ChatID int64             `json:"chatId"`
 	Text   string            `json:"text"`
 	Type   MessageType       `json:"type"`
 	Locale translator.Locale `json:"locale"`
