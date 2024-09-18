28aa1d8a217b90d652eb8a801b5aa915ae4f467b
Maksym Bilan <>
2024-09-18 14:03:13 +0300

    ux improvements

--- internal/bot/sleep_analysis.go
+++ internal/bot/sleep_analysis.go
@@ -11,6 +11,8 @@ import (
 
 // Handle the sleep analysis command
 func handleSleepAnalysis(session *Session) {
+	setOutputText("analysis_waiting", session)
+
 	userID := session.User.ID
 	note, err := firestore.LastNote(session.Context, userID)
 	if err != nil {
