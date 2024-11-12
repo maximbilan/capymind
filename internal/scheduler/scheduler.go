3a595bc9944261bbce4345d13b62f3083be276c4
Maksym Bilan <maximb.mail@gmail.com>
2024-11-12 18:20:52 +0200

    Performance improvements in scheduling messages (#58)
    * refactoring
    
    * test
    
    * fixing issues
    
    * clean up
    
    * measuring tool
    
    ---------
    
    Co-authored-by: Maksym Bilan <>

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/capymind/internal/analysis"
@@ -15,8 +16,11 @@ import (
 	"github.com/capymind/internal/translator"
 )
 
+var wg sync.WaitGroup
+
 // Schedule a message for all users
 func Schedule(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
 	log.Println("Schedule capymind...")
 
 	typeStr := r.URL.Query().Get("type")
@@ -64,70 +68,84 @@ func Schedule(w http.ResponseWriter, r *http.Request) {
 				continue
 			}
 
-			log.Printf("[Scheduler] Schedule a message for user: %s", user.ID)
-
-			userLocale := translator.Locale(*user.Locale)
-
-			var localizedMessage string
-			if messageType == WeeklyAnalysis {
-				notes, err := firestore.GetNotesForLastWeek(&ctx, user.ID)
-				if err != nil {
-					log.Printf("[Scheduler] Error getting notes from firestore, %s", err.Error())
-					continue
-				}
-
-				if len(notes) > 0 {
-					var strings []string
-					for _, note := range notes {
-						if note.Text != "" {
-							strings = append(strings, note.Text)
-						}
-					}
-					localizedMessage = *analysis.AnalyzeLastWeek(strings, userLocale, &ctx)
-				} else {
-					continue
-				}
-			} else if messageType == UserStats {
-				count, err := firestore.NotesCount(&ctx, user.ID)
-				if err != nil {
-					log.Printf("[Scheduler] Error getting notes count from firestore, %s", err.Error())
-					continue
-				}
-				// Send only if the user has more than one note in the journal
-				if count > 1 {
-					localizedMessage = fmt.Sprintf(translator.Translate(userLocale, "user_progress_message"), count)
-				} else {
-					continue
-				}
-			} else {
-				localizedMessage = translator.Translate(userLocale, message)
-			}
-
-			var scheduledTime time.Time
-			if isCloud {
-				scheduledTime = time.Now().Add(time.Duration(offset) * time.Hour)
-				scheduledTime = scheduledTime.Add(-time.Duration(*user.SecondsFromUTC) * time.Second)
-			} else {
-				// For local testing, schedule the message in 10 seconds
-				scheduledTime = time.Now().Add(10 * time.Second)
-			}
-
-			scheduledMessage := ScheduledMessage{
-				ChatID: user.ChatID,
-				Text:   localizedMessage,
-				Type:   messageType,
-				Locale: userLocale,
-			}
-
-			scheduleTask(&ctx, scheduledMessage, scheduledTime)
+			// Prepare message concurrently
+			wg.Add(1)
+			go prepareMessage(&user, &ctx, offset, messageType, message, isCloud)
 		}
 		return nil
 	})
+
+	wg.Wait()
 
 	// Close Firestore client
 	CloseTasks()
 	// Close Tasks client
 	firestore.CloseClient()
+
+	// Calculate how seconds this function takes to execute
+	elapsed := time.Since(start)
+	log.Printf("[Scheduler] Execution time for %s: %s", messageType, elapsed)
+}
+
+func prepareMessage(user *firestore.User, ctx *context.Context, offset int, messageType MessageType, message string, isCloud bool) {
+	defer wg.Done()
+
+	log.Printf("[Scheduler] Schedule a message for user: %s", user.ID)
+
+	userLocale := translator.Locale(*user.Locale)
+
+	var localizedMessage string
+	if messageType == WeeklyAnalysis {
+		notes, err := firestore.GetNotesForLastWeek(ctx, user.ID)
+		if err != nil {
+			log.Printf("[Scheduler] Error getting notes from firestore, %s", err.Error())
+			return
+		}
+
+		if len(notes) > 0 {
+			var strings []string
+			for _, note := range notes {
+				if note.Text != "" {
+					strings = append(strings, note.Text)
+				}
+			}
+			localizedMessage = *analysis.AnalyzeLastWeek(strings, userLocale, ctx)
+		} else {
+			return
+		}
+	} else if messageType == UserStats {
+		count, err := firestore.NotesCount(ctx, user.ID)
+		if err != nil {
+			log.Printf("[Scheduler] Error getting notes count from firestore, %s", err.Error())
+			return
+		}
+		// Send only if the user has more than one note in the journal
+		if count > 1 {
+			localizedMessage = fmt.Sprintf(translator.Translate(userLocale, "user_progress_message"), count)
+		} else {
+			return
+		}
+	} else {
+		localizedMessage = translator.Translate(userLocale, message)
+	}
+
+	var scheduledTime time.Time
+	if isCloud {
+		scheduledTime = time.Now().Add(time.Duration(offset) * time.Hour)
+		scheduledTime = scheduledTime.Add(-time.Duration(*user.SecondsFromUTC) * time.Second)
+	} else {
+		// For local testing, schedule the message in 10 seconds
+		scheduledTime = time.Now().Add(10 * time.Second)
+	}
+
+	scheduledMessage := ScheduledMessage{
+		ChatID: user.ChatID,
+		Text:   localizedMessage,
+		Type:   messageType,
+		Locale: userLocale,
+	}
+
+	scheduleTask(ctx, scheduledMessage, scheduledTime)
 }
 
 // Send a message to a user
