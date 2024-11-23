dca58a79732d7ccd62325618b1706fb2b9a82195
Maksym Bilan <>
2024-11-23 18:14:59 +0200

    active users count

--- internal/firestore/admin.go
+++ internal/firestore/admin.go
@@ -3,23 +3,40 @@ package firestore
 import (
 	"context"
 	"errors"
+	"time"
 
+	"cloud.google.com/go/firestore"
 	"cloud.google.com/go/firestore/apiv1/firestorepb"
 )
 
 // Get the total number of users
 func GetTotalUserCount(ctx *context.Context) (int64, error) {
-	return getTotalRecordsCount(ctx, users.String())
+	return getTotalRecordsCount(ctx, users.String(), nil)
+}
+
+// Get the number of active users (User.timestamp less than 7 days)
+func GetActiveUserCount(ctx *context.Context) (int64, error) {
+	sevenDaysAgo := time.Now().AddDate(0, 0, -7)
+	filter := func(query firestore.Query) firestore.Query {
+		return query.Where("timestamp", ">", sevenDaysAgo)
+	}
+	return getTotalRecordsCount(ctx, users.String(), filter)
 }
 
 // Get the total number of notes
 func GetTotalNoteCount(ctx *context.Context) (int64, error) {
-	return getTotalRecordsCount(ctx, notes.String())
+	return getTotalRecordsCount(ctx, notes.String(), nil)
 }
 
-// Get the total number of records in a collection
-func getTotalRecordsCount(ctx *context.Context, collection string) (int64, error) {
-	aggregationQuery := client.Collection(collection).NewAggregationQuery().WithCount("all")
+// Get the total number of records in a collection with an optional filter
+func getTotalRecordsCount(ctx *context.Context, collection string, filter func(firestore.Query) firestore.Query) (int64, error) {
+	query := client.Collection(collection).Query
+
+	if filter != nil {
+		query = filter(query)
+	}
+
+	aggregationQuery := query.NewAggregationQuery().WithCount("all")
 
 	results, err := aggregationQuery.Get(*ctx)
 	if err != nil {
