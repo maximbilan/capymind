35957442237372e8c8718b00be146763b244418c
Maksym Bilan <>
2024-12-23 18:39:34 +0200

    clean up

--- scripts/create_tag.sh
+++ scripts/create_tag.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-# Get the directory of this script
-SCRIPT_DIR=$(dirname "$0")
-
-# Path to the version.go file (adjusted for the relative path)
-VERSION_FILE="$SCRIPT_DIR/../version.go"
-
-# Check if version.go exists
-if [[ ! -f "$VERSION_FILE" ]]; then
-  echo "Error: $VERSION_FILE not found."
-  exit 1
-fi
-
-# Path to get_version.sh
-GET_VERSION_SCRIPT="$SCRIPT_DIR/get_version.sh"
-
-# Import the current version using the get_version.sh script
-CURRENT_VERSION=$("$GET_VERSION_SCRIPT")
+# Get current version
+source ./scripts/get_version.sh
+CURRENT_VERSION=$(get_version)
 
 # Create a new tag
 git tag -a "releases/$CURRENT_VERSION" -m "$CURRENT_VERSION"
