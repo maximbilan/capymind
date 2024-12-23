b529a4f38f7d9de569b7bc4bfa149a7b9beea089
Maksym Bilan <>
2024-12-23 17:59:19 +0200

    create tag script

--- scripts/create_tag.sh
+++ scripts/create_tag.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
 
+# Get the directory of this script
+SCRIPT_DIR=$(dirname "$0")
+
+# Path to the version.go file (adjusted for the relative path)
+VERSION_FILE="$SCRIPT_DIR/../version.go"
+
+# Check if version.go exists
+if [[ ! -f "$VERSION_FILE" ]]; then
+  echo "Error: $VERSION_FILE not found."
+  exit 1
+fi
+
+# Path to get_version.sh
+GET_VERSION_SCRIPT="$SCRIPT_DIR/get_version.sh"
+
+# Import the current version using the get_version.sh script
+CURRENT_VERSION=$("$GET_VERSION_SCRIPT")
+
+# Create a new tag
+git tag -a "releases/$CURRENT_VERSION" -m "$CURRENT_VERSION"
+
+# Push the tag to the remote repository
+git push origin "releases/$CURRENT_VERSION"
