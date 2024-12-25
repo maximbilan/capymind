ca1ea0346b37ba674384b7a8c65ae78973b767b2
Maksym Bilan <maximb.mail@gmail.com>
2024-12-25 11:18:28 +0200

    AI abstract layer (#86)
    Co-authored-by: Maksym Bilan <>

--- internal/analysis/analysis.go
+++ internal/analysis/analysis.go
@@ -2,13 +2,13 @@ package analysis
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"log"
 
 	"github.com/capymind/internal/translator"
-	"github.com/openai/openai-go"
+	"github.com/capymind/third_party/openai"
 )
+
+var service openai.OpenAI
 
 func AnalyzeQuickly(notes []string, locale translator.Locale, ctx *context.Context) *string {
 	return analyzeJournal(getLocalizedPrompt(QuickAnalysis, locale), notes, locale, ctx, nil)
@@ -26,7 +26,7 @@ func analyzeJournal(prompt Prompt, notes []string, locale translator.Locale, ctx
 		userPrompt += fmt.Sprintf("%d. %s ", index+1, note)
 	}
 
-	response := request("analysis", "Analysis of the user's journal entries", systemPrompt, userPrompt, ctx)
+	response := service.Request("analysis", "Analysis of the user's journal entries", systemPrompt, userPrompt, ctx)
 
 	var analysis string
 	if response != nil {
@@ -49,48 +49,6 @@ func AnalyzeSleep(text string, locale translator.Locale, ctx *context.Context) *
 	userPrompt := prompt.User
 	userPrompt += text
 
-	response := request("sleep_analysis", "Analysis of the user's sleep", systemPrompt, userPrompt, ctx)
+	response := service.Request("sleep_analysis", "Analysis of the user's sleep", systemPrompt, userPrompt, ctx)
 	return response
-}
-
-// Request an AI analysis
-func request(name string, description string, systemPrompt string, userPrompt string, ctx *context.Context) *string {
-	ai := createAI()
-
-	var responseSchema = generateSchema[Response]()
-
-	schemaParam := openai.ResponseFormatJSONSchemaJSONSchemaParam{
-		Name:        openai.F(name),
-		Description: openai.F(description),
-		Schema:      openai.F(responseSchema),
-		Strict:      openai.Bool(true),
-	}
-
-	chat, err := ai.Chat.Completions.New(*ctx, openai.ChatCompletionNewParams{
-		Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
-			openai.SystemMessage(systemPrompt),
-			openai.UserMessage(userPrompt),
-		}),
-		ResponseFormat: openai.F[openai.ChatCompletionNewParamsResponseFormatUnion](
-			openai.ResponseFormatJSONSchemaParam{
-				Type:       openai.F(openai.ResponseFormatJSONSchemaTypeJSONSchema),
-				JSONSchema: openai.F(schemaParam),
-			},
-		),
-		Model: openai.F(openai.ChatModelGPT4oMini),
-	})
-
-	response := Response{}
-	if err != nil {
-		log.Printf("[AI] Error parsing analysis: %s", err.Error())
-		return nil
-	}
-
-	err = json.Unmarshal([]byte(chat.Choices[0].Message.Content), &response)
-	if err != nil {
-		log.Printf("[AI] Error parsing analysis: %s", err.Error())
-		return nil
-	}
-
-	return &response.Text
 }
