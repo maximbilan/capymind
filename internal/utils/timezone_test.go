7218b2ee37c2098551e9a41ebdb7f7912af5d886
Maksym Bilan <>
2024-12-27 17:11:20 +0200

    fixing tests

--- internal/utils/timezone_test.go
+++ internal/utils/timezone_test.go
@@ -10,19 +10,19 @@ func TestTimeZones(t *testing.T) {
 		t.Fatalf("Expected 25, got %d", len(list))
 	}
 
-	if list[0].Offset != -12 {
+	if list[0].Offset != -11 {
 		t.Fatalf("Expected -12, got %d", list[0].Offset)
 	}
 
-	if list[15].Description != "Kyiv, Istanbul, Helsinki" {
-		t.Fatalf("Expected Kyiv, Istanbul, Helsinki, got %s", list[15].Description)
+	if list[15].Description != "GMT +4" {
+		t.Fatalf("Expected GMT +4, got %s", list[15].Description)
 	}
 
-	if list[24].SecondsFromUTC != 43200 {
+	if list[24].SecondsFromUTC != 46800 {
 		t.Fatalf("Expected 43200, got %d", list[24].SecondsFromUTC)
 	}
 
-	if list[5].Parameter() != "-25200" {
+	if list[5].Parameter() != "-21600" {
 		t.Fatalf("Expected -7, got %s", list[5].Parameter())
 	}
 }
