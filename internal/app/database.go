d799608e4d853b50f6a21f51a1e65af8e9129437
Maksym Bilan <>
2024-12-28 17:54:22 +0200

    clean up

--- internal/app/database.go
+++ internal/app/database.go
@@ -6,7 +6,6 @@ import "github.com/capymind/third_party/firestore"
 var db firestore.Firestore
 
 // Use the following storages for the database
-var adminStorage firestore.AdminStorage
 var userStorage firestore.UserStorage
 var feedbackStorage firestore.FeedbackStorage
 var noteStorage firestore.NoteStorage
