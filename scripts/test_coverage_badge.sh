e000d2feca90f0f39f05b6253f02bab31b1342f1
Maksym Bilan <maximb.mail@gmail.com>
2024-12-29 22:05:25 +0200

    Update test_coverage_badge.sh

--- scripts/test_coverage_badge.sh
+++ scripts/test_coverage_badge.sh
@@ -6,16 +6,19 @@ go tool cover -func=coverage.out
 
 COVERAGE=$(go tool cover -func=coverage.out | grep total | awk '{print $3}' | sed 's/%//')
 
-COLOR="red"
-if (( $(echo "$COVERAGE >= 90" | bc -l) )); then
-  COLOR="brightgreen"
-elif (( $(echo "$COVERAGE >= 75" | bc -l) )); then
-  COLOR="yellow"
+if [ "$COVERAGE" -ge 80 ]; then
+  COLOR="brightgreen" # High coverage
+elif [ "$COVERAGE" -ge 50 ]; then
+  COLOR="yellow" # Medium coverage
+elif [ "$COVERAGE" -ge 25 ]; then
+  COLOR="orange" # Low-medium coverage
+else
+  COLOR="red" # Very low coverage
 fi
 
-echo "<svg xmlns='http://www.w3.org/2000/svg' width='120' height='20'>
-  <rect width='60' height='20' fill='#555'/>
-  <rect x='60' width='60' height='20' fill='#${COLOR}'/>
-  <text x='30' y='14' fill='#fff' font-family='Verdana' font-size='11' text-anchor='middle'>Test Coverage</text>
-  <text x='90' y='14' fill='#fff' font-family='Verdana' font-size='11' text-anchor='middle'>${COVERAGE}%</text>
+echo "<svg xmlns='http://www.w3.org/2000/svg' width='150' height='20'>
+  <rect width='100' height='20' fill='#555'/>
+  <rect x='100' width='50' height='20' fill='${COLOR}'/>
+  <text x='50' y='14' fill='#fff' font-family='Verdana' font-size='11' text-anchor='middle'>Test Coverage</text>
+  <text x='125' y='14' fill='#fff' font-family='Verdana' font-size='11' text-anchor='middle'>${COVERAGE}%</text>
 </svg>" > .badges/test_coverage.svg
