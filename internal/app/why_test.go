5a2b9eb4b1b96812e65454d54e1bac7d4cab7317
Maksym Bilan <>
2024-12-29 14:23:41 +0200

    more tests

---
+++ internal/app/why_test.go
@@ -0,0 +1,16 @@
+package app
+
+import (
+	"testing"
+
+	"github.com/capymind/internal/database"
+)
+
+func TestWhyHandler(t *testing.T) {
+	session := createSession(&Job{Command: "/why"}, &database.User{}, nil)
+	handleWhy(session)
+
+	if session.Job.Output[0].TextID != "why_descr" {
+		t.Errorf("Expected output text to be 'why_descr', got %s", session.Job.Output[0].TextID)
+	}
+}
