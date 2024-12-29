ab961876c79202e8f7bb6d2d50d9cf2d69716d3b
Maksym Bilan <maximb.mail@gmail.com>
2024-12-29 21:54:27 +0200

    Update test_coverage_badge.sh

--- scripts/test_coverage_badge.sh
+++ scripts/test_coverage_badge.sh
@@ -16,6 +16,6 @@ fi
 echo "<svg xmlns='http://www.w3.org/2000/svg' width='120' height='20'>
   <rect width='60' height='20' fill='#555'/>
   <rect x='60' width='60' height='20' fill='#${COLOR}'/>
-  <text x='30' y='14' fill='#fff' font-family='Verdana' font-size='11' text-anchor='middle'>coverage</text>
+  <text x='30' y='14' fill='#fff' font-family='Verdana' font-size='11' text-anchor='middle'>Test Coverage</text>
   <text x='90' y='14' fill='#fff' font-family='Verdana' font-size='11' text-anchor='middle'>${COVERAGE}%</text>
 </svg>" > .badges/test_coverage.svg
