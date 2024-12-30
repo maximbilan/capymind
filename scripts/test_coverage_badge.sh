6f9333ef3e7e6423feeaa442b878621389c4bb36
Maksym Bilan <>
2024-12-30 13:25:11 +0200

    Update Test Coverage polishing

--- scripts/test_coverage_badge.sh
+++ scripts/test_coverage_badge.sh
@@ -6,22 +6,25 @@ go tool cover -func=coverage.out
 
 COVERAGE=$(go tool cover -func=coverage.out | grep total | awk '{print $3}' | sed 's/%//')
 
-if [ "$COVERAGE" -ge 80 ]; then
-  COLOR="brightgreen" # High coverage
-elif [ "$COVERAGE" -ge 50 ]; then
-  COLOR="yellow" # Medium coverage
-elif [ "$COVERAGE" -ge 25 ]; then
-  COLOR="orange" # Low-medium coverage
+# Green color if coverage is 75% or more
+# Yellow color if coverage is 50% or more (but white text should be visible)
+# Otherwise, red color
+if [ $(echo "$COVERAGE >= 75" | bc) -eq 1 ]; then
+  COLOR="#4c1"
+elif [ $(echo "$COVERAGE >= 50" | bc) -eq 1 ]; then
+  COLOR="#dfb317"
 else
-  COLOR="red" # Very low coverage
+  COLOR="#e05d44"
 fi
 
 echo "<svg xmlns='http://www.w3.org/2000/svg' width='150' height='20'>
   <rect width='150' height='20' fill='#555' rx='5' ry='5'/>
-  <rect x='100' width='50' height='20' fill='${COLOR}' rx='5' ry='5' mask='url(#round-right)'/>
-  <mask id='round-right'>
-    <rect x='100' width='50' height='20' fill='white' rx='5' ry='5'/>
-  </mask>
+  <defs>
+    <clipPath id='clip-rounded'>
+      <rect width='150' height='20' rx='5' ry='5'/>
+    </clipPath>
+  </defs>
+  <rect x='100' width='50' height='20' fill='${COLOR}' clip-path='url(#clip-rounded)'/>
   <text x='50' y='14' fill='#fff' font-family='Verdana' font-size='11' text-anchor='middle'>Test Coverage</text>
   <text x='125' y='14' fill='#fff' font-family='Verdana' font-size='11' text-anchor='middle'>${COVERAGE}%</text>
 </svg>" > .badges/test_coverage.svg
