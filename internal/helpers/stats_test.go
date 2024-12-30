88c079f9a3378d131a75d6abf7d15e9c784312d5
Maksym Bilan <>
2024-12-30 09:49:04 +0200

    mocks clean up

--- internal/helpers/stats_test.go
+++ internal/helpers/stats_test.go
@@ -4,65 +4,13 @@ import (
 	"context"
 	"testing"
 
-	"github.com/capymind/internal/database"
+	"github.com/capymind/internal/mocks"
 	"github.com/capymind/internal/translator"
 )
 
-type adminStatsMock struct{}
-
-func (storage adminStatsMock) GetTotalUserCount(ctx *context.Context) (int64, error) {
-	return 100, nil
-}
-
-func (storage adminStatsMock) GetActiveUserCount(ctx *context.Context) (int64, error) {
-	return 75, nil
-}
-
-func (storage adminStatsMock) GetTotalNoteCount(ctx *context.Context) (int64, error) {
-	return 999, nil
-}
-
-type feedbackStatsMock struct{}
-
-func (storage feedbackStatsMock) GetFeedbackForLastWeek(ctx *context.Context) ([]database.UserFeedback, error) {
-	var array []database.UserFeedback
-
-	firstName := "John"
-	lastName := "Doe"
-	user := database.User{
-		ID:        "1",
-		FirstName: &firstName,
-		LastName:  &lastName,
-	}
-	feedback1 := database.Feedback{
-		Text: "Test feedback",
-	}
-	feedback2 := database.Feedback{
-		Text: "Test feedback 2",
-	}
-
-	userFeedback1 := database.UserFeedback{
-		User:     user,
-		Feedback: feedback1,
-	}
-	userFeedback2 := database.UserFeedback{
-		User:     user,
-		Feedback: feedback2,
-	}
-
-	array = append(array, userFeedback1)
-	array = append(array, userFeedback2)
-
-	return array, nil
-}
-
-func (storage feedbackStatsMock) NewFeedback(ctx *context.Context, user database.User, feedback database.Feedback) error {
-	return nil
-}
-
 func TestGetStats(t *testing.T) {
-	adminStorage := adminStatsMock{}
-	feedbackStorage := feedbackStatsMock{}
+	adminStorage := mocks.AdminStorageMock{}
+	feedbackStorage := mocks.FeedbackStorageMock{}
 
 	context := context.Background()
 	stats := GetStats(&context, translator.EN, adminStorage, feedbackStorage)
---
+++ internal/mocks/feedback_mock.go
@@ -0,0 +1,45 @@
+package mocks
+
+import (
+	"context"
+
+	"github.com/capymind/internal/database"
+)
+
+type FeedbackStorageMock struct{}
+
+func (storage FeedbackStorageMock) GetFeedbackForLastWeek(ctx *context.Context) ([]database.UserFeedback, error) {
+	var array []database.UserFeedback
+
+	firstName := "John"
+	lastName := "Doe"
+	user := database.User{
+		ID:        "1",
+		FirstName: &firstName,
+		LastName:  &lastName,
+	}
+	feedback1 := database.Feedback{
+		Text: "Test feedback",
+	}
+	feedback2 := database.Feedback{
+		Text: "Test feedback 2",
+	}
+
+	userFeedback1 := database.UserFeedback{
+		User:     user,
+		Feedback: feedback1,
+	}
+	userFeedback2 := database.UserFeedback{
+		User:     user,
+		Feedback: feedback2,
+	}
+
+	array = append(array, userFeedback1)
+	array = append(array, userFeedback2)
+
+	return array, nil
+}
+
+func (storage FeedbackStorageMock) NewFeedback(ctx *context.Context, user database.User, feedback database.Feedback) error {
+	return nil
+}
