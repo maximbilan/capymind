686be03bdfb54ddb2e5bde67ae8198036d0f7828
Maksym Bilan <maximb.mail@gmail.com>
2025-10-18 21:39:17 +0300

    Agent token

--- scripts/generate_agent_token.sh
+++ scripts/generate_agent_token.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+SERVICE_ACCOUNT=$CAPY_SERVICE_ACCOUNT
+PROJECT_ID=$CAPY_PROJECT_ID
+
+gcloud iam service-accounts add-iam-policy-binding $SERVICE_ACCOUNT \
+  --role=roles/iam.serviceAccountTokenCreator \
+  --member=serviceAccount:$SERVICE_ACCOUNT \
+  --project=$PROJECT_ID
+
+gcloud iam service-accounts keys create key.json \
+  --iam-account=$SERVICE_ACCOUNT
+
+# Activate the service account
+gcloud auth activate-service-account --key-file=key.json
+
+# Generate the identity token
 gcloud auth print-identity-token
